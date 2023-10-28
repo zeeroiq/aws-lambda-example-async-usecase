b0a475e6956c0eaab569bbcf82feba93463a5352
zeeroiq <shrikantvishwakarma@outlook.com>
2023-10-29 03:44:24 +0530

    log4j support for logging. closes #4

--- patient-checkout/src/main/java/com/zeero/async/usecase/apis/BillManagementLambda.java
+++ patient-checkout/src/main/java/com/zeero/async/usecase/apis/BillManagementLambda.java
@@ -1,23 +1,23 @@
 package com.zeero.async.usecase.apis;
 
-import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.google.gson.Gson;
 import com.zeero.async.usecase.apis.event.PatientCheckoutEvent;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class BillManagementLambda {
 
     private final Gson gson = new Gson();
-    public void handler(SNSEvent snsEvent, Context context) {
+    public void handler(SNSEvent snsEvent) {
         snsEvent.getRecords().parallelStream().forEach(record -> {
             try {
 
                 PatientCheckoutEvent patientCheckoutEvent = gson.fromJson(record.getSNS().getMessage(), PatientCheckoutEvent.class);
-                context.getLogger().log("Consumed message from PATIENT_CHECKOUT_EVENT topic");
-
+                log.info("Consumed message from PATIENT_CHECKOUT_EVENT topic");
+                log.info(gson.toJson(patientCheckoutEvent));
             } catch (Exception e) {
-                context.getLogger().log("Error occurred ");
-                context.getLogger().log(e.getLocalizedMessage());
+                log.error("Error occurred \n", e);
             }
         });
     }
--- patient-checkout/src/main/java/com/zeero/async/usecase/apis/PatientCheckoutLambda.java
+++ patient-checkout/src/main/java/com/zeero/async/usecase/apis/PatientCheckoutLambda.java
@@ -9,41 +9,38 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.zeero.async.usecase.apis.event.PatientCheckoutEvent;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * Handler for requests to Lambda function.
  */
+@Slf4j
 public class PatientCheckoutLambda {
 
     private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
     private final AmazonSNS sns = AmazonSNSClientBuilder.defaultClient();
     private final Gson gson = new Gson();
 
-    public void handleRequest(S3Event s3Event, final Context context) {
-        LambdaLogger logger = context.getLogger();
+    public void handleRequest(S3Event s3Event) {
         try {
         s3Event.getRecords().parallelStream().forEach(record -> {
             S3ObjectInputStream content = s3.getObject(record.getS3().getBucket().getName(),
                             record.getS3().getObject().getKey()).getObjectContent();
             InputStreamReader jsonEventContent = new InputStreamReader(content);
             List<PatientCheckoutEvent> checkoutEventsPatients = Arrays.asList(gson.fromJson(jsonEventContent, PatientCheckoutEvent[].class));
-            logger.log(checkoutEventsPatients.toString());
+            log.info(checkoutEventsPatients.toString());
 
             checkoutEventsPatients
                     .parallelStream()
                     .forEach(checkoutEvent -> sns.publish(System.getenv("PATIENT_CHECKOUT_TOPIC"), gson.toJson(checkoutEvent)));
         });
         } catch (Exception e) {
-            logger.log("Error occurred ");
-            logger.log(Arrays.toString(e.getStackTrace()));
+            log.error("Error occurred \n", e);
         }
     }
 
